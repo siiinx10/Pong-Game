a8b6551cc0da47dbd148f01932eff33c29850930
Manarsh Nowbut <91077872+siiinx10@users.noreply.github.com>
2024-01-27 21:26:16 -0330

    Add files via upload

--- pong.py
+++ pong.py
@@ -10,6 +10,10 @@ FPS = 60
 WHITE = (255,255,255)
 BLACK =(0,0,0)
 
+#SCORE_FONT = pygame.font.SysFont("comicsans", 50)
+
+BALL_RADIUS = 7
+
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 
 class Paddle:
@@ -32,12 +36,84 @@ class Paddle:
         else:
             self.y += self.VEL
 
+        
+class Ball:
 
-def draw(win,paddles):
+    MAX_VEL = 5
+    COLOR = WHITE
+
+    def __init__(self, x , y, radius):
+        self.x = x
+        self.y = y 
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+    #Moving the ball
+
+    
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+
+def draw(win,paddles, ball, left_score, right_score):
     win.fill(BLACK)
+
+    #Draw score
+    #left_score_text = SCORE_FONT.render(f"{left_score}", 1, WHITE)
+    #right_score_text = SCORE_FONT.render(f"{right_score}", 1, WHITE)
+    #win.blit(left_score_text, (WIDTH//4 - left_score_text.get_width()//2, 20))
+    #win.blit(right_score_text,(WIDTH * (3/4) - right_score_text.get_width()//2, 20))
+
+
     for paddle in paddles:
         paddle.draw(win)
+
+    #Drawing a dash line in the middle
+    for i in range (10, HEIGHT, HEIGHT//20):
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
+    
+    ball.draw(win)
+
+    
     pygame.display.update()
+
+def handle_collision(ball, left_paddle, right_paddle):
+    if ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+
+    if ball.x_vel < 0:
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+
+                middle_y = left_paddle.y + left_paddle.height/2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height / 2) / ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = y_vel * -1
+
+    else:
+        #right paddes
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
+
+
+                middle_y = right_paddle.y + right_paddle.height/2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (right_paddle.height / 2) / ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = y_vel * -1
 
 def handle_paddles_movement(keys, left_paddle, right_paddle):
     if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0 :
@@ -55,12 +131,21 @@ def main():
     clock = pygame.time.Clock()
 
     left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
-    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT )
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    ball = Ball(WIDTH // 2, HEIGHT // 2, BALL_RADIUS)
+
+    #Implementing score
+    if ball.x < 0:
+        right_score += 1
+
+    elif ball.x > WIDTH:
+        left_score += 1
 
 
     while run:
         clock.tick(FPS)
-        draw(WIN, [left_paddle, right_paddle])
+        draw(WIN, [left_paddle, right_paddle], ball, left_score, right_score)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -68,6 +153,8 @@ def main():
 
         keys = pygame.key.get_pressed()
         handle_paddles_movement(keys, left_paddle, right_paddle)
+        ball.move()
+        handle_collision(ball, left_paddle, right_paddle)
 
     pygame.quit()
 
